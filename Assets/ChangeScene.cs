2993382339baa9667219b89754d5556bf25d06c8
BaburMB <babur.mb@live.com>
2016-03-19 15:55:17 +0600

    First UI

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Used this for change scene
+	public void ChangeToScene (string where){
+		Application.LoadLevel (where);
+	}
+
+}
