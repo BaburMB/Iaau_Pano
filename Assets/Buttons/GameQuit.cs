0b411a1439a49a42e913d097a61258f967aa032a
BaburMB <babur.mb@live.com>
2016-04-18 23:54:21 +0600

    Pinch and Zoom feature init

--- Assets/Buttons/GameQuit.cs
+++ Assets/Buttons/GameQuit.cs
@@ -10,6 +10,8 @@ public class GameQuit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+			Application.Quit(); 
 	}
-}
+	}
+
